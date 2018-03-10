6fdaf4c8022f0544cd9f63761ab38346b65c1b69
Str1204 <33195779+Abaniko58@users.noreply.github.com>
2018-03-10 10:38:28 +0200

    Add files via upload
    my html and css files for project

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// Google Map
+let map;
+
+
+// Update UI's markers
+function update()
+{
+    // Get map's bounds
+    let bounds = map.getBounds();
+    let ne = bounds.getNorthEast();
+    let sw = bounds.getSouthWest();
+
+    // Get places within bounds (asynchronously)
+    let parameters = {
+        ne: `${ne.lat()},${ne.lng()}`,
+        q: $("#q").val(),
+        sw: `${sw.lat()},${sw.lng()}`
+    };
+    $.getJSON("/update", parameters, function(data, textStatus, jqXHR) {
+
+       // Remove old markers from map
+       removeMarkers();
+
+       // Add new markers to map
+       for (let i = 0; i < data.length; i++)
+       {
+           addMarker(data[i]);
+       }
+    });
+};
